9a1922784ebf91e891e021eea489ec401887be23
kwkhaw <kwkhaw@gmail.com>
2015-08-28 00:20:45 +0800

    Initial Commit

---
+++ SwiftAtlas/SwiftAtlas.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAtlas.h
+//  SwiftAtlas
+//
+//  Created by kw on 28/8/15.
+//  Copyright © 2015 layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftAtlas.
+FOUNDATION_EXPORT double SwiftAtlasVersionNumber;
+
+//! Project version string for SwiftAtlas.
+FOUNDATION_EXPORT const unsigned char SwiftAtlasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAtlas/PublicHeader.h>
+
+
